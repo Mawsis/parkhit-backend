19b64ec725b5580d43b31157a38e15233bdd0f5b
Mawsis <Mawsis448@outlook.com>
2025-01-02 00:18:53 +0100

    feat: add Redis client configuration and test endpoint for Redis connectivity

---
+++ src/config/redis.ts
@@ -0,0 +1,18 @@
+import { createClient } from 'redis';
+
+const redisClient = createClient({
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+    },
+});
+
+redisClient.on('error', (err) => {
+    console.error('Redis Client Error', err);
+});
+
+(async () => {
+    await redisClient.connect();
+})();
+
+export default redisClient;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,24 @@ import { Router } from "express";
 import authRoutes from "./authRoutes";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../docs/swagger.json";
+import redisClient from "../config/redis";
 
 const router = Router();
 
 router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use("/auth", authRoutes);
+
+
+
+router.get('/test-redis', async (req, res) => {
+    try {
+        await redisClient.set('test', 'Redis is working!');
+        const value = await redisClient.get('test');
+        res.status(200).send(`Redis says: ${value}`);
+    } catch (error) {
+        res.status(500).send('Redis test failed');
+    }
+});
+
 
 export default router;
